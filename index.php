c777d5aae26fd55cb213345792e37046452e3d5b
Leonid <kievskiyleon@gmail.com>
2017-11-14 20:02:59 +0200

    My commit

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?
-$orgs = [
-    'org1' => [
-        'worker1' => ['name' => 'Jone',
-            'age' => '22'],
-        'worker2' => ['name' => 'Jone',
-            'age' => '22'],
-        'worker3' => ['name' => 'Jone',
-            'age' => '22'],
-        'worker4' => ['name' => 'Jone',
-            'age' => '22']
-    ]
-];
-//$array(“1”=>”1”,”2”=>”2”,3=>3);
-echo 'Name '.$orgs['org1']['worker1']['name'].' age '.$orgs['org1']['worker1']['age']
+
+//var_dump($_GET);
+$a=$_GET.'wkebf';
+echo ($a);
 ?>
