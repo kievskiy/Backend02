c22cf9ef9393c299b9227a15b36eeec87ba5a490
Leonid <kievskiyleon@gmail.com>
2017-11-19 21:18:07 +0200

    My commit

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <?php
-//Задача 2: Реализуйте функцию вывода положительных значений массива кратных «5».
+//Инициализируйте массив с целочисленными значениями.
+//Реализуйте функцию записи положительных и отрицательных значений в разные файлы использую оператор *switch*
 $array = [12, -5, 234, -10, 10, 45, 46, 9, 3, 7, 1, 15, 0];
 echo "Исходный массив:</br>";
 foreach ($array as $value) {
     echo "$value</br>";
 }
-echo "Значения положительные и кратные 5:</br>";
-arrayCountAliquot($array, 5);
-function arrayCountAliquot($array, $multiplicity)
+
+arrayInTwoFiles($array);
+function arrayInTwoFiles($array)
 {
+    file_put_contents('positive.txt', '');
+    file_put_contents('negative.txt', '');
+
     foreach ($array as $value) {
-        If ((($value % $multiplicity) == 0) and ($value >= 0)) {
-            echo "$value</br>";
+        switch ($value < 0) {
+            case true:
+                file_put_contents('negative.txt', $value . '  ', FILE_APPEND | LOCK_EX);
+                break;
+            case false:
+                file_put_contents('positive.txt', $value . '  ', FILE_APPEND | LOCK_EX);
+                break;
         }
     }
     return true;
