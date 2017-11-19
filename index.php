f668d6b5a2ebb2d597b361d256bbf6f7a5a25793
Leonid <kievskiyleon@gmail.com>
2017-11-19 20:23:27 +0200

    My commit

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 //Задача 2: Реализуйте функцию вывода положительных значений массива кратных «5».
-$array = [12, 234, -10, 10, 45, 46, 9, 3, 7, 1, 15, 0];
+$array = [12, -5, 234, -10, 10, 45, 46, 9, 3, 7, 1, 15, 0];
 echo "Исходный массив:</br>";
 foreach ($array as $value) {
     echo "$value</br>";
 }
-echo "Значения кратные 5:</br>";
+echo "Значения положительные и кратные 5:</br>";
 arrayCountAliquot($array, 5);
 function arrayCountAliquot($array, $multiplicity)
 {
