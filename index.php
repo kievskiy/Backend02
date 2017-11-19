7a0f579c013c45bb402de7724b49bf63613b3b1f
Leonid <kievskiyleon@gmail.com>
2017-11-19 18:38:07 +0200

    My commit

--- index.php
+++ index.php
@@ -1,45 +1,14 @@
 <?php
-//$array = [2, 124, 12, 67, 'Number'=>58, 11, 12, 15, 11, 12, 23, 45];
-//asort($array, SORT_NUMERIC);
-//If (array_key_exists('Number', $array)) {
-//    echo 'Массив содержит ключ Number';
-//};
-//var_dump(array_key_exists('Number', $array));
-//$result=array_unique($array);
-//var_dump($array);
-//echo '</br>';
-//var_dump($result);
-//echo '</br>';
-//var_dump(array_values($result));
-//$text = "Текст  \n";
-
-//$fr = fopen("file.txt","w");
-//fwrite($fr,$text);
-//fclose($fr);
-//file_put_contents("file.txt","<p>Текст1</p>",FILE_APPEND | LOCK_EX);
-//file_put_contents("file1.txt","Текст2  ",FILE_APPEND | LOCK_EX);
-//echo(file_get_contents("file.txt"));
-$fileName = "example.txt";
-$fileDate = "<p>Мой текст</p>";
-$rights = "a+";
-/**
- * Put date to some file with some rights
- * @param string $fileName
- * @param $fileDate
- * @param string (optional) $rights
- * @return bool
- */
-function filePutContent($fileName, $fileDate, $rights = "w")
+//Задача 1: Реализуйте функцию подсчета количества элементов в массиве.
+$array = [12, 234, 'value', 45, 9, 3, 7, 1, 0];
+function arrayElementCount($array)
 {
-    $fp = fopen($fileName, $rights);
-    fwrite($fp, $fileDate);
-    fclose($fp);
-    return true;
+    $array_count = 0;
+    foreach ($array as $value) {
+        $array_count++;
+    }
+    return $array_count;
 }
+echo arrayElementCount($array);
 
-if (filePutContent($fileName, $fileDate/*,$rights*/)) {
-    echo 'Save complite';
-} else {
-    echo "Save failed";
-};
 ?>
