c416b6f99fbe7115edffa9eda95e684e887ff9d4
Leonid <kievskiyleon@gmail.com>
2017-11-19 18:46:36 +0200

    My commit

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <?php
-//Задача 1: Реализуйте функцию подсчета количества элементов в массиве.
-$array = [12, 234, 'value', 45, 9, 3, 7, 1, 0];
-function arrayElementCount($array)
+//Задача 2: Реализуйте функцию вывода положительных значений массива кратных «5».
+$array = [12, 234, 10, 45, 9, 3, 7, 1, 15, 0];
+echo arrayCountAliquot($array, 5);
+function arrayCountAliquot($array, $multiplicity)
 {
-    $array_count = 0;
+    $CountAliquot = 0;
     foreach ($array as $value) {
-        $array_count++;
+        echo "$value</br>";
+        If (($value % $multiplicity) === 0) {
+//            echo "$value</br>";
+            $CountAliquot++;
+        }
+        return $CountAliquot;
     }
-    return $array_count;
 }
-echo arrayElementCount($array);
+
+
+
+
 
 ?>
