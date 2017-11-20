a4d66117b8234f9a3a3fa2235d8c183a3cdd498a
Leonid <kievskiyleon@gmail.com>
2017-11-20 16:18:48 +0200

    My commit

--- index.php
+++ index.php
@@ -2,32 +2,29 @@
 //Инициализируйте массив с целочисленными значениями.
 //Реализуйте функцию записи положительных и отрицательных значений в разные файлы использую оператор *switch*
 $array = [12, -5, 234, -10, 10, 45, 46, 9, 3, 7, 1, 15, 0];
-echo "Исходный массив:</br>";
-foreach ($array as $value) {
-    echo "$value</br>";
-}
+var_dump($array);
+echo "<br/>";
+for ($i = 0; $i < (count($array)); $i++) {
 
-arrayInTwoFiles($array);
-function arrayInTwoFiles($array)
-{
-    file_put_contents('positive.txt', '');
-    file_put_contents('negative.txt', '');
-
-    foreach ($array as $value) {
-        switch ($value < 0) {
-            case true:
-                file_put_contents('negative.txt', $value . '  ', FILE_APPEND | LOCK_EX);
-                break;
-            case false:
-                file_put_contents('positive.txt', $value . '  ', FILE_APPEND | LOCK_EX);
-                break;
+    for ($j = 0; ($j < (count($array)  - $i)); $j++) {
+//        echo $j . '  ';
+     /*   echo $array[count($array) - $j] . '  ';*/
+        if ($array[$j] < $array[$j - 1]) {
+            $pr = $array[$j];
+            $array[$j] = $array[$j - 1];
+            $array[$j - 1] = $pr;
         }
+//        echo (count($array) - 1 - $j) . '  ';
+//        if ($array[$j] > $array[$j + 1]) {
+//            $b = $array[$j];
+//            $array[$j] = $array[$j - 1];
+//            $array[$j - 1] = $b;
+//        }
+//        echo "<br/>";
     }
-    return true;
+   /* echo "<br/>";*/
 }
 
-echo('Положительные элементы массима  ' . file_get_contents('positive.txt'));
-
-echo('   Отрицательные  элементы массима  ' . file_get_contents('negative.txt'));
+var_dump($array);
 
 ?>
