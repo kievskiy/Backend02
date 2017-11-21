ab1d45b0f34152c76e71974b0891517eb78390fe
Leonid <kievskiyleon@gmail.com>
2017-11-21 06:36:44 +0200

    My commit

--- index.php
+++ index.php
@@ -5,24 +5,13 @@ $array = [12, -5, 234, -10, 10, 45, 46, 9, 3, 7, 1, 15, 0];
 var_dump($array);
 echo "<br/>";
 for ($i = 0; $i < (count($array)); $i++) {
-
-    for ($j = 0; ($j < (count($array)  - $i)); $j++) {
-//        echo $j . '  ';
-     /*   echo $array[count($array) - $j] . '  ';*/
+    for ($j = 0; ($j < (count($array) - $i)); $j++) {
         if ($array[$j] < $array[$j - 1]) {
             $pr = $array[$j];
             $array[$j] = $array[$j - 1];
             $array[$j - 1] = $pr;
         }
-//        echo (count($array) - 1 - $j) . '  ';
-//        if ($array[$j] > $array[$j + 1]) {
-//            $b = $array[$j];
-//            $array[$j] = $array[$j - 1];
-//            $array[$j - 1] = $b;
-//        }
-//        echo "<br/>";
     }
-   /* echo "<br/>";*/
 }
 
 var_dump($array);
