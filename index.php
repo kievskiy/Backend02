113d0c10911064881c98a5ed59c5cb849150e414
Leonid <kievskiyleon@gmail.com>
2017-11-24 14:59:01 +0200

    first callback function.

--- index.php
+++ index.php
@@ -1,43 +1,49 @@
 <?php
-$arrayCount = [1,2,3,4,5];
+$array = [12, 3435, 'trade', 47, 87, 22, 'Nikon', 71, 'yellow', 48, 'bitter'];
 
-$userFunc = function ($result) {
-    // $result исходные данные которые мы передали при вызове call_user_func
-
-    //Увеличения исходных данных на 1 и запись с новым клюючем
-    foreach ($result['current'] as $val) {
-        $result['new'][] = $val + 1;
+$funcOdd = function ($var) {
+    for ($i = 0; $i < (count($var)); $i++) {
+        // является ли элемент массива нечетным
+        if ($var[$i] % 2 !== 0) {
+            $result[] = $var[$i];
+        }
     }
-
     return $result;
 };
 
-// Вызывает нашу основую функцию. И передаем в нее два параметра:
-// Исходный массив с данными и пользовательскую функцию для их обработки.
-$response = myFunction($arrayCount, $userFunc);
+$funcEven = function ($var) {
+    for ($i = 0; $i < (count($var)); $i++) {
+        // является ли элемент массива четным
+        if ($var[$i] % 2 == 0) {
+            $result[] = $var[$i];
+        }
+    }
+    return $result;
+};
 
-echo "<pre>";
-var_dump($response);
-echo "</pre>";
+function arrayToInt($array1, $callback = null)
+{
+    // В новый массив помещаем только числовые значения
+    for ($i = 0; $i < count($array1); $i++) {
+        If (is_numeric($array1[$i])) {
+            $result[] = $array1 [$i];
+        }
+    }
 
-/**
- *
- * @param $arrayCount Исходный массив
- * @param $callback Функция для дополнтительной обработки исходного массива
- * @return mixed
- */
- function myFunction($arrayCount, $callback) {
-        // Записывает исходный массив в переменную $result['current']
-        $result['current'] = $arrayCount;
+    //Проверяем является ли переданный параметр функцией.
+    if (is_callable($callback)) {
+        //Вызываем переданную пользовательскую функцию и возвращаем обработанные данные
+        $result = call_user_func($callback, $result);
+    }
+    return $result;
+}
 
-     //Проверяем является ли переданный параметр функцией.
-     if (is_callable($callback)) {
-         //Вызываем переданную пользовательскую функцию и возвращаем обработанные данные
-         $result = call_user_func($callback, $result);
-     }
-
-     //Возвращаем финальный результат.
-
-     return $result;
- }
+echo('Исходный массив <br>');
+var_dump($array);
+echo('<br><br> Массив состоящий только из чисел (фильтр исходного)<br>');
+var_dump(arrayToInt($array));
+echo('<br><br> Нечётные числа массива <br>');
+var_dump(arrayToInt($array, $funcOdd));
+echo('<br><br> Чётные числа массива <br>');
+var_dump(arrayToInt($array, $funcEven));
 ?>
